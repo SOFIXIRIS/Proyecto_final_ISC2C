84fa4b697c787a2585585cec32eab0ae51b523eb
BasherMX <66893712+BasherMX@users.noreply.github.com>
2020-06-16 09:23:37 -0500

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Proyect_Final_ISC2C
+//
+//  Created by Brayan Ulises Basher on 6/16/20.
+//  Copyright © 2020 Brayan Ulises Vázquez Heredia. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
